3ab6deda69e34e9821071acbd4a0f28de3898938
Ganesh <krganesh1611@gmail.com>
2021-06-03 20:40:39 +0530

    Resolve issue of Row and Column name

--- Excel/script.js
+++ Excel/script.js
@@ -1,19 +1,24 @@
 $(document).ready(function () {
-    
-    let ans = "";
-    
-    let n = 28;
-    
-    while(n > 0){
-        let rem = n % 26;
-        if(rem == 0){
-            ans = "Z" + ans;
-            n = Math.floor(n/26) - 1;
-        } else {
-            ans = String.fromCharCode(rem-1+65) + ans;
-            n = Math.floor(n/26);
+    let cellContainer = $('.input-cell-container');
+
+    for (let i = 1; i <= 100; i++) {
+        let ans = "";
+        let n = i;
+
+        while (n > 0) {
+            let rem = n % 26;
+            if (rem == 0) {
+                ans = "Z" + ans;
+                n = Math.floor(n / 26) - 1;
+            } else {
+                ans = String.fromCharCode(rem - 1 + 65) + ans;
+                n = Math.floor(n / 26);
+            }
         }
+        let column = $(`<div class="column-name colCode-${ans}" id="colId-${i}">${ans}</div>`);
+        $(".column-name-container").append(column);
+
+        let row = $(`<div class="row-name" id="rowId-${i}">${i}</div>`);
+        $(".row-name-container").append(row);
     }
-    console.log(ans);
-    
 });
