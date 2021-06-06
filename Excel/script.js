bc418ee5be412bd27272c9988f439a247f90368f
Ganesh <krganesh1611@gmail.com>
2021-06-06 22:55:04 +0530

    Designed formula bar and made icon & cell selectable creating formula bar and give designing of selcectable class

--- Excel/script.js
+++ Excel/script.js
@@ -21,13 +21,28 @@ $(document).ready(function () {
         $(".row-name-container").append(row);
     }
 
-    for(let i = 1; i <= 100; i++) {
+    for (let i = 1; i <= 100; i++) {
         let row = $(`<div class="cell-row"></div>`);
-        for(let j = 1; j <= 100; j++) {
+        for (let j = 1; j <= 100; j++) {
             let colCode = $(`.colId-${j}`).attr("id").split("-")[1];
-            let column = $(`<div class="input-cell" contenteditable="true" id = "row-${i}-col-${j}" data="code-${colCode}"></div>`);
+            let column = $(`<div class="input-cell" contenteditable="false" id = "row-${i}-col-${j}" data="code-${colCode}"></div>`);
             row.append(column);
         }
         $(".input-cell-container").append(row);
     }
+
+
+    $(".align-icon").click(function () {
+        $(".align-icon.selected").removeClass("selected");
+        $(this).addClass("selected");
+    })
+
+    $(".style-icon").click(function (){
+        $(this).toggleClass("selected");
+    })
+
+    $(".input-cell").click(function() {
+        $(".input-cell.selected").removeClass("selected");
+        $(this).addClass("selected");
+    })
 });
